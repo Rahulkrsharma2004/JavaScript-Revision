eaf96aad2182068bc3527073728f2b0f4c67e748
Rahulkrsharma2004 <kumarrahulbasdiha@gmail.com>
2024-12-18 15:51:38 +0530

    array-obj

--- 18-12-2024/output.js
+++ 18-12-2024/output.js
@@ -21,19 +21,40 @@
 //   }, 2000);
 // }, 3000);
 
-let count = 0;
-const data = {
-  a: [1, 2, 3],
-  b: [4, 5, 6],
-  c: [7, 8, 9],
+// let count = 0;
+// const data = {
+//   a: [1, 2, 3],
+//   b: [4, 5, 6],
+//   c: [7, 8, 9],
+// };
+
+// function increment() {
+//   const [arr1, arr2, arr3] = Object.values(data);
+//   setInterval(() => {
+//     console.log(arr1[count], arr2[count], arr3[count]);
+//     count++;
+//   }, 1000);
+// }
+
+// increment();
+
+const obj = {
+  a: 1,
+  b: 2,
+  c: {
+    d: 3,
+    e: 4,
+  },
 };
 
-function increment() {
-  const [arr1, arr2, arr3] = Object.values(data);
-  setInterval(() => {
-    console.log(arr1[count], arr2[count], arr3[count]);
-    count++;
-  }, 1000);
-}
+const arr = [5, 6, [7, 8]];
 
-increment();
+let result = [];
+
+const myFunc = ({ a, b }, [x, y, [z, w]], { c: { d, e } }) => {
+  const temp = [a, b, x, y, z, w, d, e];
+  result.push(temp);
+};
+
+myFunc(obj, arr, obj);
+console.log(result);
