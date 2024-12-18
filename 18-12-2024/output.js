0501574192c0a82a17dc3e124ee8f9f3107f10c7
Rahulkrsharma2004 <kumarrahulbasdiha@gmail.com>
2024-12-18 16:21:46 +0530

    Let,Const,Var,Scope,Hoisting

--- 18-12-2024/output.js
+++ 18-12-2024/output.js
@@ -38,23 +38,99 @@
 
 // increment();
 
-const obj = {
-  a: 1,
-  b: 2,
-  c: {
-    d: 3,
-    e: 4,
-  },
-};
+// const obj = {
+//   a: 1,
+//   b: 2,
+//   c: {
+//     d: 3,
+//     e: 4,
+//   },
+// };
 
-const arr = [5, 6, [7, 8]];
+// const arr = [5, 6, [7, 8]];
 
-let result = [];
+// let result = [];
 
-const myFunc = ({ a, b }, [x, y, [z, w]], { c: { d, e } }) => {
-  const temp = [a, b, x, y, z, w, d, e];
-  result.push(temp);
-};
+// const myFunc = ({ a, b }, [x, y, [z, w]], { c: { d, e } }) => {
+//   const temp = [a, b, x, y, z, w, d, e];
+//   result.push(temp);
+// };
 
-myFunc(obj, arr, obj);
-console.log(result);
+// myFunc(obj, arr, obj);
+// console.log(result);
+
+// const promise1 = Promise.resolve("One");
+// console.log(promise1);
+// const promise2 = new Promise((resolve) =>
+//   setTimeout(() => resolve("Two"), 1000)
+// );
+// const promise3 = Promise.reject("Three");
+// Promise.allSettled([promise1, promise2, promise3]).then((results) =>
+//   console.log(results)
+// );
+
+// class Bird {
+//     constructor(name) {
+//     this.name = name;
+//     }
+//     speak() {
+//     console.log(`${this.name} makes a noise.`);
+//     }
+// }
+// class Crow extends Bird{
+//     speak() {
+//     super.speak();
+//     console.log(`${this.name} sings.`);
+//     }
+// }
+// const crow = new Crow("Tim");
+// crow.speak();
+
+// let x = 0;
+// function increment() {
+//   x++;
+//   console.log(x);
+//   if (x < 3) {
+//     let x = 0;
+//     increment();
+//   }
+// }
+// increment();
+
+// for (var i = 0; i < 3; i++) {
+//   setTimeout(function () {
+//     console.log(i);
+//   }, 0);
+// }
+// for (let i = 0; i < 3; i++) {
+//   setTimeout(function () {
+//     console.log(i);
+//   }, 0);
+// }
+
+// let a = 1;
+// function foo() {
+//   let a = 2;
+//   console.log(a, "first a");
+//   function bar() {
+//     let a = 3;
+//     console.log(a);
+//     console.log(a);
+//   }
+//   bar();
+// }
+// foo();
+// console.log(a);
+
+
+// var a = 1;
+// (function() {
+//   console.log(a);
+//   var a = 2;
+//   console.log(a);
+// })();
+// console.log(a);
+
+const arr = [1, 2, 3];
+const [a, ...rest] = arr;
+console.log(a, rest);
