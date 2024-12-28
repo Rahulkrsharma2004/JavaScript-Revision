188c67b64b0366028fa86efda06cf72ce176bc38
Rahulkrsharma2004 <kumarrahulbasdiha@gmail.com>
2024-12-28 23:51:07 +0530

    some output code

--- 28-12-2024/output.js
+++ 28-12-2024/output.js
@@ -1,14 +1,34 @@
 console.log([] + []);
 // its give emapty string
 
-var a = [1, 2, 3];
-var b = a;
-b.push(4);
-console.log(a);
+// var a = [1, 2, 3];
+// var b = a;
+// b.push(4);
+// console.log(a);
 
-var foo = function() {
-    return {
-        bar: "hello"
-    };
-};
-console.log(foo().bar);
+// var foo = function() {
+//     return {
+//         bar: "hello"
+//     };
+// };
+// console.log(foo().bar);
+
+// let x = {};
+// let y = x;
+// x.a = 1;
+// console.log(y.a);
+
+// let a = 10;
+// let b = a;
+// a = 20;
+// console.log(a);
+// console.log(b);
+
+
+let a = {};
+let b = {key: 'b'};
+let c = {key: 'c'};
+a[b] = 123;
+a[c] = 456;
+console.log(a[b]);
+
