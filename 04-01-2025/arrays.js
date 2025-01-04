c79695a2d1692e1129a921895aa0372d2a571f24
Rahulkrsharma2004 <kumarrahulbasdiha@gmail.com>
2025-01-04 19:12:58 +0530

    some code added

---
+++ 04-01-2025/arrays.js
@@ -0,0 +1,29 @@
+// const arr = [1, 2, 3, 4];
+// const result = arr.map((num, index) => num * index);
+// console.log(result);
+
+
+// const fruits = ['apple', 'banana', 'cherry'];
+// const result = fruits.includes('grape'); // this item is present in array or not ?
+// console.log(result);
+
+
+// const arr = [10, 20, 30];
+// const result = arr.reduce((sum, num) => sum + num, 0);
+// console.log(result);
+
+
+
+// const arr = [1, 2, [3, 4], [5, [6, 7]]];
+// const flattened = arr.flat(2);
+// console.log(flattened);
+
+// const numbers = [1, 2, 3, 4, 5];
+// const result = numbers.filter(num => num % 2 === 0);
+// console.log(result);
+
+
+const numbers = [1, 2, 3, 4, 5];
+const result = numbers.reduceRight((acc, num) => acc + num, 0);
+console.log(result);
+
