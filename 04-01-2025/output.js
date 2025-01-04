0baa30faac260f92963ab954022b28f416fc8dc1
Rahulkrsharma2004 <kumarrahulbasdiha@gmail.com>
2025-01-04 19:06:40 +0530

    some code added

---
+++ 04-01-2025/output.js
@@ -0,0 +1,15 @@
+// const arr = [1, 2, 3, 4];
+// const result = arr.map((num, index) => num * index);
+// console.log(result);
+
+
+// const fruits = ['apple', 'banana', 'cherry'];
+// const result = fruits.includes('grape'); // this item is present in array or not ?
+// console.log(result);
+
+
+const arr = [10, 20, 30];
+const result = arr.reduce((sum, num) => sum + num, 0);
+console.log(result);
+
+
