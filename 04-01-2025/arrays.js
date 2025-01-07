ad6e5ca6d6f77a94361f732b25cf7e2816322512
Rahulkrsharma2004 <kumarrahulbasdiha@gmail.com>
2025-01-07 20:18:30 +0530

    added a something in code

--- 04-01-2025/arrays.js
+++ 04-01-2025/arrays.js
@@ -32,3 +32,7 @@ const arr = [1, 2, 3, 4];
 const result = arr.sort((a, b) => b - a).slice(1).reverse();
 console.log(result);
 
+
+const array = [1,2,3,4,5,6,7,8,9,10,11,12]
+const results = array.sort((a, b) => a - b)
+console.log(results);
